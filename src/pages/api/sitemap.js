8db5ca137c01b5b35ffee517824ecf5ca1b05823
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-10-29 09:13:57 +0500

    fix sitemap

--- src/pages/api/sitemap.js
+++ src/pages/api/sitemap.js
@@ -1,21 +1,23 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const sitemaps = [
-    'https://instruhub.com/sitemap-products.xml',
-    'https://instruhub.com/sitemap-categories.xml',
-    'https://instruhub.com/sitemap-brands.xml',
+    'https://safemedsupply.com/ae/catalog/sitemap-medical.xml',
+    'https://safemedsupply.com/ae/catalog/sitemap-safety.xml',
+    'https://safemedsupply.com/sa/catalog/sitemap-medical.xml',
+    'https://safemedsupply.com/sa/catalog/sitemap-safety.xml',
   ];
 
   let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
   xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
   sitemaps.forEach((sitemap) => {
-    xml += '<sitemap>\n';
-    xml += `<loc>${sitemap}</loc>\n`;
-    xml += '</sitemap>\n';
+    xml += '  <sitemap>\n';
+    xml += `    <loc>${sitemap}</loc>\n`;
+    xml += '  </sitemap>\n';
   });
 
   xml += '</sitemapindex>';
-  
+
+  // Set header and send XML response
   res.setHeader('Content-Type', 'text/xml');
   res.status(200).send(xml);
 }
